d9fb724fede3c5d55d67edbc7f67a772a3ba1370
h8m0ss <danarmenta32@gmail.com>
2021-07-14 12:33:46 -0500

    added customization options for size of logo and text color

--- lib/common/store_display.dart
+++ lib/common/store_display.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:video_game_wish_list/models/store_model.dart';
 
 class StoreDisplay extends StatelessWidget {
-  const StoreDisplay({Key? key, required this.model}) : super(key: key);
+  const StoreDisplay({
+    Key? key,
+    required this.model,
+    this.width,
+    this.height,
+    this.textColor,
+  }) : super(key: key);
 
   final StoreModel model;
+  final double? width;
+  final double? height;
+  final Color? textColor;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +22,16 @@ class StoreDisplay extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
-          width: 50,
+          width: width,
+          height: height,
           child: Image.network(model.icon),
         ),
         Padding(
           padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-          child: Text(model.name),
+          child: Text(
+            model.name,
+            style: TextStyle(color: textColor),
+          ),
         ),
         SizedBox(width: 50),
       ],
