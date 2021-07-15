fe9b5f139b4eba8eabe926d105d633c15636182c
h8m0ss <danarmenta32@gmail.com>
2021-07-15 14:08:14 -0500

    Finished filter model implementation

--- lib/models/filter_model.dart
+++ lib/models/filter_model.dart
@@ -1,11 +1,33 @@
+import 'package:video_game_wish_list/models/store_model.dart';
+
 class FilterModel {
   const FilterModel({
-    this.higherPrice,
+    this.upperPrice,
     this.lowerPrice,
     this.stores: const [],
+    this.sorting: DealSorting.Rating,
+    this.isDescending: false,
+    this.metaCriticScore,
+    this.steamScore,
   });
 
-  final int? higherPrice;
+  final int? upperPrice;
   final int? lowerPrice;
-  final List<int> stores;
+  final List<StoreModel> stores;
+  final DealSorting sorting;
+  final bool isDescending;
+  final int? metaCriticScore;
+  final int? steamScore;
+}
+
+enum DealSorting {
+  Rating, // default
+  Title,
+  Savings,
+  Price,
+  MetaCritic,
+  Reviews,
+  Release,
+  Store,
+  Recent,
 }
