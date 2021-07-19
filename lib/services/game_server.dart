28653de1fca28f7210192ae3b04d5608af6d7821
h8m0ss <danarmenta32@gmail.com>
2021-07-19 00:18:40 -0500

    added simple filtering funcitonality

--- lib/services/game_server.dart
+++ lib/services/game_server.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:video_game_wish_list/models/deal_model.dart';
+import 'package:video_game_wish_list/models/filter_model.dart';
 import 'package:video_game_wish_list/models/store_model.dart';
 
 class GameServer {
@@ -15,8 +16,10 @@ class GameServer {
 
   /// asynchronously fetch the games from the cheap shark api from the specified
   /// [page]
-  Future<DealResults> fetchGames(int page) async {
-    final response = await _dio.get('$dealsUrl?pageNumber=$page');
+  Future<DealResults> fetchGames(int page, FilterModel filter,
+      [String search = '']) async {
+    String query = _getQuery(page, filter, search);
+    final response = await _dio.get('$dealsUrl$query');
     if (response.statusCode == 200) {
       List<dynamic> items = response.data;
       print(response.headers.value('X-Total-Page-Count'));
@@ -65,6 +68,55 @@ class GameServer {
       }
     } else
       throw HttpException('Could not reach deals API ${response.statusCode}');
+  }
+
+  String _getQuery(int page, FilterModel filter, String search) {
+    if (page == 0 && filter.isDefault && search.isEmpty) return '';
+    var result = '?';
+    if (page != 0) result += 'pageNumber=$page&';
+    if (!filter.isDescending) result += 'desc=1&';
+    if (filter.lowerPrice != null) result += 'lowerPrice=${filter.lowerPrice}&';
+    if (filter.metacriticScore != null)
+      result += 'metacritic=${filter.metacriticScore}&';
+    if (filter.sorting != DealSorting.Rating)
+      result += 'sortBy=${_getSortTitle(filter.sorting)}&';
+    if (filter.steamScore != null)
+      result += 'steamRating=${filter.steamScore}&';
+    if (filter.stores.length != 0) {
+      var storeText = filter.stores
+          .fold('', (previousValue, element) => '$previousValue${element.id},');
+      storeText = storeText.substring(0, storeText.length - 2);
+
+      result += 'storeID=$storeText&';
+    }
+    if (filter.upperPrice != null) result += 'upperPrice=${filter.upperPrice}&';
+
+    print(result.substring(0, result.length - 1));
+
+    return result.substring(0, result.length - 1);
+  }
+
+  String _getSortTitle(DealSorting sorting) {
+    switch (sorting) {
+      case DealSorting.Metacritic:
+        return 'Metacritic';
+      case DealSorting.Price:
+        return 'Price';
+      case DealSorting.Rating:
+        return 'DealRating';
+      case DealSorting.Title:
+        return 'Title';
+      case DealSorting.Savings:
+        return 'Savings';
+      case DealSorting.Reviews:
+        return 'Reviews';
+      case DealSorting.Release:
+        return 'Release';
+      case DealSorting.Store:
+        return 'Store';
+      case DealSorting.Recent:
+        return 'recent';
+    }
   }
 }
 
