207b60b386cab02a9a596c08c61b198f847da3ac
h8m0ss <danarmenta32@gmail.com>
2021-07-19 00:22:50 -0500

    moved home page state to its own file

--- lib/app/home_page/home_page_bloc.dart
+++ lib/app/home_page/home_page_bloc.dart
@@ -6,75 +6,67 @@ import 'package:video_game_wish_list/models/filter_model.dart';
 import 'package:video_game_wish_list/services/game_server.dart';
 
 import 'home_page_event.dart';
+import 'home_page_state.dart';
 
 class HomePageBloc extends Bloc<HomePageEvent, HomePageState> {
   HomePageBloc(
     HomePageState initialState, {
     required this.server,
   }) : super(initialState) {
-    getPage0();
+    _getPage0();
   }
 
   final GameServer server;
-  int currentPage = 0;
-  int totalPages = 1;
+  int _currentPage = 0;
+  int _totalPages = 1;
 
-  bool get hasMorePages => currentPage < totalPages - 1;
+  bool get hasMorePages => _currentPage < _totalPages - 1;
 
   @override
   Stream<HomePageState> mapEventToState(HomePageEvent event) async* {
     if (event is AppendDealsEvent)
-      yield state.updateWith(deals: [...state.deals, ...event.deals]);
-    if (event is SetDealsEvent)
+      yield state.updateWith(deals: [...state.deals ?? [], ...event.deals]);
+    else if (event is SetDealsEvent)
       yield state.updateWith(deals: event.deals);
+    else if (event is SetFilterEvent) {
+      yield state.updateWith(filter: event.filter);
+      add(GetInitialPageEvent());
+    } else if (event is GetInitialPageEvent) {
+      add(SetDealsEvent(null));
+      await _getPage0();
+    } else if (event is RenderItemEvent)
+      _onRender(event.index);
+    else if (event is FilterButtonPressedEvent)
+      _onFiltering(event.context);
     else
       throw UnimplementedError();
   }
 
-  void onRender(int i) async {
-    int threshold = (currentPage + 1) * 60 - 5;
+  void _onRender(int i) async {
+    int threshold = (_currentPage + 1) * 60 - 5;
     print('rendering $i');
     if (i >= threshold) {
       print('Past threshold!');
       if (hasMorePages) {
-        currentPage++;
-        final items = await server.fetchGames(currentPage);
-        totalPages = items.totalPages;
+        _currentPage++;
+        final items = await server.fetchGames(_currentPage, state.filter);
+        _totalPages = items.totalPages;
         add(AppendDealsEvent(items.results));
       }
     }
   }
 
-  Future<void> getPage0() async {
-    final page0 = await server.fetchGames(0);
-    currentPage = 0;
-    totalPages = page0.totalPages;
+  Future<void> _getPage0() async {
+    final page0 = await server.fetchGames(0, state.filter);
+    _currentPage = 0;
+    _totalPages = page0.totalPages;
     add(SetDealsEvent(page0.results));
   }
 
-  Future<void> onFiltering(BuildContext context) async {
+  Future<void> _onFiltering(BuildContext context) async {
     final filter = await FilterBottomSheet.show(context, state.filter);
+    if (filter != null) {
+      add(SetFilterEvent(filter));
+    }
   }
-}
-
-class HomePageState {
-  HomePageState({
-    this.deals: const [],
-    this.filter: const FilterModel(),
-  });
-
-  final List<DealModel> deals;
-  final FilterModel filter;
-
-  HomePageState updateWith({
-    List<DealModel>? deals,
-    FilterModel? filter,
-  }) {
-    return HomePageState(
-      deals: deals ?? this.deals,
-      filter: filter ?? this.filter,
-    );
-  }
-
-  int get dealCount => deals.length;
 }
---
+++ lib/app/home_page/home_page_state.dart
@@ -0,0 +1,24 @@
+import 'package:video_game_wish_list/models/deal_model.dart';
+import 'package:video_game_wish_list/models/filter_model.dart';
+
+class HomePageState {
+  HomePageState({
+    this.deals,
+    this.filter: const FilterModel(),
+  });
+
+  final List<DealModel>? deals;
+  final FilterModel filter;
+
+  HomePageState updateWith({
+    List<DealModel>? deals,
+    FilterModel? filter,
+  }) {
+    return HomePageState(
+      deals: deals ?? this.deals,
+      filter: filter ?? this.filter,
+    );
+  }
+
+  int get dealCount => deals?.length ?? 0;
+}
