5427db3e7ce33a700ae68337931c440f0b18ecaa
h8m0ss <danarmenta32@gmail.com>
2021-07-19 00:21:55 -0500

    Added update with and equality operator

--- lib/models/filter_model.dart
+++ lib/models/filter_model.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:video_game_wish_list/models/store_model.dart';
 
 class FilterModel {
@@ -6,18 +10,63 @@ class FilterModel {
     this.lowerPrice,
     this.stores: const [],
     this.sorting: DealSorting.Rating,
-    this.isDescending: false,
-    this.metaCriticScore,
+    this.isDescending: true,
+    this.metacriticScore,
     this.steamScore,
   });
+
+  FilterModel updateWith(
+      {int? upperPrice,
+      int? lowerPrice,
+      List<StoreModel>? stores,
+      DealSorting? sorting,
+      bool? isDescending,
+      int? metacriticScore,
+      int? steamScore}) {
+    return FilterModel(
+      isDescending: isDescending ?? this.isDescending,
+      lowerPrice: lowerPrice ?? this.lowerPrice,
+      metacriticScore: metacriticScore ?? this.metacriticScore,
+      sorting: sorting ?? this.sorting,
+      steamScore: steamScore ?? this.steamScore,
+      stores: stores ?? this.stores,
+      upperPrice: upperPrice ?? this.upperPrice,
+    );
+  }
 
   final int? upperPrice;
   final int? lowerPrice;
   final List<StoreModel> stores;
   final DealSorting sorting;
   final bool isDescending;
-  final int? metaCriticScore;
+  final int? metacriticScore;
   final int? steamScore;
+
+  bool get isDefault => FilterModel() == this;
+
+  @override
+  operator ==(dynamic other) {
+    return (other != null &&
+        other is FilterModel &&
+        other.isDescending == isDescending &&
+        other.lowerPrice == lowerPrice &&
+        other.metacriticScore == metacriticScore &&
+        other.sorting == sorting &&
+        other.steamScore == steamScore &&
+        listEquals(other.stores, stores) &&
+        other.upperPrice == upperPrice);
+  }
+
+  @override
+  int get hashCode => hashValues(
+        isDescending,
+        lowerPrice,
+        metacriticScore,
+        sorting,
+        steamScore,
+        hashList(stores),
+        upperPrice,
+      );
 }
 
 enum DealSorting {
@@ -25,7 +74,7 @@ enum DealSorting {
   Title,
   Savings,
   Price,
-  MetaCritic,
+  Metacritic,
   Reviews,
   Release,
   Store,
