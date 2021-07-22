de475e1adc52219cd82e94a7816e16d2db448763
h8m0ss <danarmenta32@gmail.com>
2021-07-22 13:03:04 -0500

    added simple documentation

--- lib/common/other/double_extension.dart
+++ lib/common/other/double_extension.dart
@@ -1,6 +1,15 @@
 import 'dart:math';
 
 extension toString on double {
+  /// Returns the double as a string with [n] or less decimal places
+  ///
+  /// ```
+  /// 0.999999999.toStringAsDynamic(2); // '0.99'
+  ///
+  /// 4.23.toStringAsDynamic(7); // '4.23'
+  ///
+  /// 2.0000000032.toStringAsDynamic(3); // '2'
+  /// ```
   String toStringAsDynamic(int n) {
     double currentNumber = this;
     int tens = pow(10, n) as int;
